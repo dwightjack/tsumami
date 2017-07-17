02d3d30913125ce52a9f9f1d4c5a43291b27e579
Marco Solazzi <marco.solazzi@gmail.com>
2017-07-17 22:41:49 +0200

    tests

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -41,4 +41,242 @@ describe('utils.js', () => {
         });
 
     });
+
+    describe('`isNumeric()`', () => {
+
+        //https://github.com/jquery/jquery/blob/master/test/unit/core.js
+        it('should return `true` on negative integer string', () => {
+            expect(utils.isNumeric('-10')).toBe(true);
+        });
+
+        it('should return `true` on zero string', () => {
+            expect(utils.isNumeric('0')).toBe(true);
+        });
+
+        it('should return `true` on positive integer string', () => {
+            expect(utils.isNumeric('5')).toBe(true);
+        });
+
+        it('should return `true` on negative integer number', () => {
+            expect(utils.isNumeric(-16)).toBe(true);
+        });
+
+        it('should return `true` on zero integer number', () => {
+            expect(utils.isNumeric(0)).toBe(true);
+        });
+
+        it('should return `true` on positive integer number', () => {
+            expect(utils.isNumeric(32)).toBe(true);
+        });
+
+        it('should return `true` on negative floating point string', () => {
+            expect(utils.isNumeric('-1.6')).toBe(true);
+        });
+
+        it('should return `true` on positive floating point string', () => {
+            expect(utils.isNumeric('4.536')).toBe(true);
+        });
+
+        it('should return `true` on negative floating point number', () => {
+            expect(utils.isNumeric(-2.6)).toBe(true);
+        });
+
+        it('should return `true` on positive floating point number', () => {
+            expect(utils.isNumeric(3.1415)).toBe(true);
+        });
+
+        it('should return `true` on very precise floating point number', () => {
+            expect(utils.isNumeric(1.5999999999999999)).toBe(true);
+        });
+
+        it('should return `true` on exponential notation', () => {
+            expect(utils.isNumeric(8e5)).toBe(true);
+        });
+
+        it('should return `true` on exponential notation string', () => {
+            expect(utils.isNumeric('123e-2')).toBe(true);
+        });
+
+        it('should return `true` on legacy octal integer literal string', () => {
+            expect(utils.isNumeric('040')).toBe(true);
+        });
+
+        it('should return `true` on hexadecimal integer literal string (0x...)', () => {
+            expect(utils.isNumeric('0xFF')).toBe(true);
+        });
+
+        it('should return `true` on hexadecimal integer literal string (0X...)', () => {
+            expect(utils.isNumeric('0Xba')).toBe(true);
+        });
+
+        it('should return `true` on hexadecimal integer literal', () => {
+            expect(utils.isNumeric(0xFFF)).toBe(true);
+        });
+
+        it('Empty string', () => {
+            expect(utils.isNumeric('')).toBe(false);
+        });
+
+        it('Whitespace characters string', () => {
+            expect(utils.isNumeric('        ')).toBe(false);
+        });
+
+        it('Tab characters string', () => {
+            expect(utils.isNumeric('\t\t')).toBe(false);
+        });
+
+        it('Alphanumeric character string', () => {
+            expect(utils.isNumeric('abcdefghijklm1234567890')).toBe(false);
+        });
+
+        it('Non-numeric character string', () => {
+            expect(utils.isNumeric('xabcdefx')).toBe(false);
+        });
+
+        it('Boolean true literal', () => {
+            expect(utils.isNumeric(true)).toBe(false);
+        });
+
+        it('Boolean false literal', () => {
+            expect(utils.isNumeric(false)).toBe(false);
+        });
+
+        it('Number with preceding non-numeric characters', () => {
+            expect(utils.isNumeric('bcfed5.2')).toBe(false);
+        });
+
+        it('Number with trailing non-numeric characters', () => {
+            expect(utils.isNumeric('7.2acdgs')).toBe(false);
+        });
+
+        it('Undefined value', () => {
+            expect(utils.isNumeric(undefined)).toBe(false);
+        });
+
+        it('Null value', () => {
+            expect(utils.isNumeric(null)).toBe(false);
+        });
+
+        it('NaN value', () => {
+            expect(utils.isNumeric(NaN)).toBe(false);
+        });
+
+        it('Infinity primitive', () => {
+            expect(utils.isNumeric(Infinity)).toBe(false);
+        });
+
+        it('Positive Infinity', () => {
+            expect(utils.isNumeric(Number.POSITIVE_INFINITY)).toBe(false);
+        });
+
+        it('Negative Infinity', () => {
+            expect(utils.isNumeric(Number.NEGATIVE_INFINITY)).toBe(false);
+        });
+
+        it('Empty object', () => {
+            expect(utils.isNumeric({})).toBe(false);
+        });
+
+        it('Empty array', () => {
+            expect(utils.isNumeric([])).toBe(false);
+        });
+
+        it('Array with one number', () => {
+            expect(utils.isNumeric([42])).toBe(false);
+        });
+
+        it('Instance of a function', () => {
+            expect(utils.isNumeric(() => {})).toBe(false);
+        });
+
+        it('Instance of a Date', () => {
+            expect(utils.isNumeric(new Date())).toBe(false);
+        });
+    });
+
+
+    describe('`parseString()`', () => {
+
+        it('should accept just string values', () => {
+
+            const ps = utils.parseString;
+
+            expect(ps([])).toBe(undefined);
+            expect(ps({})).toBe(undefined);
+            expect(ps(10)).toBe(undefined);
+            expect(ps(true)).toBe(undefined);
+            expect(ps(null)).toBe(undefined);
+            expect(ps(undefined)).toBe(undefined);
+            expect(ps('string')).toBeA('string');
+        });
+
+        it('should return string value', () => {
+            expect(utils.parseString('test')).toBe('test');
+        });
+
+        it('should return trimmed string value', () => {
+            expect(utils.parseString('   trim   ')).toBe('trim');
+        });
+
+        it('should parse a numeric value into a float', () => {
+            expect(utils.parseString('10')).toBe(10);
+            expect(utils.parseString('10.20')).toBe(10.2);
+            expect(utils.parseString('8e5')).toBe(8e5);
+        });
+
+        it('should parse string booleans in "real" booleans', () => {
+            expect(utils.parseString('true')).toBe(true);
+            expect(utils.parseString('false')).toBe(false);
+        });
+
+        it('should parse JSON strings', () => {
+
+            const obj = { test: 'stub', prop: 10 };
+            const str = JSON.stringify(obj);
+
+            expect(utils.parseString(str)).toMatch(obj);
+        });
+    });
+
+
+    describe('`arrayFrom()`', () => {
+
+        let legacy;
+        beforeEach(() => {
+            legacy = typeof Array.from !== 'function';
+        });
+
+        if (!legacy) {
+            it('should proxy to `Array.from` when available', () => {
+
+                if (Array.from) {
+                    const spy = expect.spyOn(Array, 'from');
+                    const arr = [];
+
+                    utils.arrayFrom(arr);
+
+                    expect(spy).toHaveBeenCalledWith(arr);
+
+                    Array.from.restore();
+                }
+
+            });
+        }
+
+        if (legacy) {
+            it('should proxy to `Array.prototype.slice` on legacy browsers', () => {
+
+                const spy = expect.spyOn(Array.prototype, 'slice').andCallThrough();
+                const arr = [];
+
+                utils.arrayFrom(arr);
+
+                expect(spy.calls[0].context).toBe(arr);
+
+                Array.prototype.slice.restore();
+
+            });
+        }
+
+    });
 });
